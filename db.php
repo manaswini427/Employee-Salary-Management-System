f13e793ef683dfdee6ca54cb200d1b175792d7ea
manaswini427 <manaswinidoll04@gmail.com>
2026-04-14 14:18:11 +0530

    Updated database connection

--- db.php
+++ db.php
@@ -1,5 +1,11 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "salary_management");
+$host = "autorack.proxy.rlwy.net";
+$user = "root";
+$password = "tqxVIcWYgbkqzOXPTzJGdWAnkpSEdTWT";
+$db = "railway";
+$port = 44088;
+
+$conn = mysqli_connect($host, $user, $password, $db, $port);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
